d856d88113d7367d17b20fcb07c06607c4a81565
Prince Narteh <prinarteh@gmail.com>
2021-07-03 08:11:55 +0000

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const [input, setInput] = useState<string>("");
+  const [code, setCode] = useState("");
+
+  const onClick = () => {
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <textarea
+        name=""
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
